890d2470cf97648bebd30255ebf30c76cda2db4d
vanguo996 <guowang939@gmail.com>
2020-11-27 20:40:20 +0800

    add types

---
+++ go.mod
@@ -0,0 +1,5 @@
+module k8s-custom-controller
+
+go 1.15
+
+require k8s.io/apimachinery v0.19.4
---
+++ pkg/apis/samplecrd/v1/register.go
@@ -0,0 +1,31 @@
+package v1
+
+import (
+	"k8s-custom-controller/pkg/apis/samplecrd"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+// GroupVersion is the identifier for the API which includes
+// the name of the group and the version of the API
+var SchemeGroupVersion = schema.GroupVersion{
+	Group:   samplecrd.GroupName,
+	Version: samplecrd.Version,
+}
+
+
+
+// addKnownTypes adds our types to the API scheme by registering
+// Network and NetworkList
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(
+		SchemeGroupVersion,
+		&Network{},
+		&NetworkList{},
+	)
+
+	// register the type in the scheme
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
